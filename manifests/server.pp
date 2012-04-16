a32c90d17573d517904e88aaef35960c4b683b2e
Uwe Stuehler <ustuehler@team.mobile.de>
2012-04-16 11:16:31 +0200

    Style guide, documentation tweaks

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,8 +1,6 @@
-# Set up an FAI server for installing Linux in a fully automated fashion.
-# A future goal for this class is to support operating systems other than
-# Linux as the server.
+# Set up an FAI server
 #
-# This class has been initially developed on Debian "squeeze".  Any other
+# This class has been initially developed on Debian 6.x (squeeze).  Other
 # versions and other Debian-based distributions such as Ubuntu may or may
 # not work out of the box.  However, since FAI setup involves many things
 # and I've recently expericed many problems with setting up FAI on Ubuntu,
@@ -12,23 +10,28 @@
 # == Parameters
 #
 # - suite: The distribution name of the Debian system (e.g. sarge, etch,
-#   lenny, sid). Default: "squeeze"
+#   lenny, sid). Default: 'squeeze'
 #
 # - mirror: The URL of a Debian mirror to retrieve packages from.  The
 #   URL scheme can be http, file or ssh.
 #
-class fai::server($suite = "squeeze", $mirror =
-    "http://ftp.debian.org/debian")
+class fai::server($suite = 'squeeze',
+  $mirror = 'http://ftp.debian.org/debian')
 {
-	case $operatingsystem {
-		Debian: { $class = 'fai::server::debian' }
-		default: { fail("$operatingsystem is currently unsupported") }
-	}
+  case $::operatingsystem {
+    Debian: {
+      $class = 'fai::server::debian'
+    }
 
-	class {
-	    $class:
-		suite => $suite,
-		mirror => $mirror;
+    default: {
+      fail("${::operatingsystem} is currently unsupported")
+    }
+  }
+
+  class {
+    $class:
+      suite  => $suite,
+      mirror => $mirror;
 
 # XXX: It's not the responsibility of this class to set up a DHCP server.
 #
@@ -38,19 +41,19 @@ class fai::server($suite = "squeeze", $mirror =
 #
 # On the other hand, we could provide a wrapper class for convenience?
 #
-#	    'fai::server::dhcpd':
-#		network => $network,
-#		netmask => $netmask,
-#		domain => $domain,
-#		domain_name_servers => $domain_name_servers,
-#		routers => $routers;
-	}
+#    'fai::server::dhcpd':
+#      network             => $network,
+#      netmask             => $netmask,
+#      domain              => $domain,
+#      domain_name_servers => $domain_name_servers,
+#      routers             => $routers;
+  }
 
-	require $class
-	require fai::server::interfaces
-	require fai::server::configdir
-	require fai::server::dnsmasq
-	#require fai::server::dhcpd
-	require fai::server::nfs
-	require fai::server::nat
+  require $class
+  require fai::server::interfaces
+  require fai::server::configdir
+  require fai::server::dnsmasq
+  #require fai::server::dhcpd
+  require fai::server::nfs
+  require fai::server::nat
 }
